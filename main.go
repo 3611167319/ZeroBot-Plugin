5401af25f933d55bb9e310414ce53d26c7c0d349
XXC <106686781+3611167319@users.noreply.github.com>
2022-07-04 17:28:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ import (
 	//                               vvvv                               //
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ai_false"       // 服务器监控
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"         // 随机老婆
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"          随机老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/b14"            // base16384加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baidu"           百度一下
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"        查询b站用户信息
@@ -84,7 +84,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/image_finder"  / 关键字搜图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/inject"         // 注入指令
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jandan"         // 煎蛋网无聊图
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/juejuezi"       // 绝绝子生成器
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/juejuezi"        绝绝子生成器
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/lolicon"        // lolicon 随机图片
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/midicreate"     // 简易midi音乐制作
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/moyu"            摸鱼
@@ -99,7 +99,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/qqwife"         // 一群一天一夫一妻制群老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/reborn"          投胎
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/runcode"        // 在线运行代码
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao"       // 以图搜图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao"        以图搜图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/scale"          // 叔叔的AI二次元图片放大
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/score"          // 分数
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime"        来份涩图
@@ -108,14 +108,14 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tarot"          // 抽塔罗牌
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tiangou"         舔狗日记
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tracemoe"        搜番
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation"    // 翻译
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation"     翻译
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/vtb_quotation"  // vtb语录
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wangyiyun"       网易云音乐热评
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/word_count"      聊天热词
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"         // 猜单词
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"          // 月幕galgame
 
-	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wtf"             鬼东西
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wtf"             //鬼东西
 
 	//                               ^^^^                               //
 	//                          ^^^^^^^^^^^^^^                          //
